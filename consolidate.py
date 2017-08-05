51f6f19039c176bb193ae7b2321d4c89eb165187
NegatioN <joakimrishaug@gmail.com>
2017-08-06 00:25:48 +0200

    add fixing of mp3 metadata

--- consolidate.py
+++ consolidate.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 import youtube_dl
 from gmusicapi import Musicmanager
 import argparse
+import eyed3
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--link', dest="link", help='A link to port', required=True)
 parser.add_argument('--auth', dest="auth", default="gmusic.cred", help='Path to oauth-file')
-#parser.add_argument('--artist', dest="artist", help='Name of artist of song', required=True)
-#parser.add_argument('--title', dest="title", help='Name of song', required=True)
+parser.add_argument('--artist', dest="artist", help='Name of artist of song', required=True)
+parser.add_argument('--title', dest="title", help='Name of song', required=True)
 config = parser.parse_args()
 
 class Youtube:
     def __init__(self, savepath):
         self.savepath = savepath
         ydl_opts = {
-            "outtmpl": "{}/%(title)s.%(ext)s".format(savepath),
+            "outtmpl": "{}/single.%(ext)s".format(savepath),
             'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
@@ -30,18 +30,18 @@ class Youtube:
 #TODO extract metadata for song automagically?
 
     def download_audio(self, link):
-        info = self.ydl.extract_info(link, download=False)
         self.ydl.download([link])
-        #TODO because skråstrek i navnet. fix
-        return "{}/{}.mp3".format(self.savepath, info['title'])
 
 
 class GoogleMusic:
     def __init__(self):
         self.api = Musicmanager()
-        if not self.api.login(oauth_credentials=config.auth):
-            self.api.perform_oauth(storage_filepath=config.auth, open_browser=True)
-        self.api.login(oauth_credentials=config.auth)
+        try:
+            if not self.api.login(oauth_credentials=config.auth):
+                self.api.perform_oauth(storage_filepath=config.auth, open_browser=True)
+                self.api.login(oauth_credentials=config.auth)
+        except:
+            pass
 
     def upload_audio(self, filepath, transcode_quality='320k'):
         self.api.upload(filepath, enable_matching=False, transcode_quality=transcode_quality)
@@ -49,10 +49,19 @@ class GoogleMusic:
 
 
 def main():
-    yt = Youtube(".")
+    savepath = "."
+    yt = Youtube(savepath)
     gm = GoogleMusic()
-    filename = yt.download_audio(config.link)
-    gm.upload_audio(filename)
+    yt.download_audio(config.link)
+    add_audio_metadata("{}/single.mp3".format(savepath), config.artist, config.title)
+    gm.upload_audio("{}/single.mp3".format(savepath))
+
+
+def add_audio_metadata(filename, artist, title):
+    audiofile = eyed3.load(filename)
+    audiofile.tag.artist = artist
+    audiofile.tag.title = title
+    audiofile.tag.save()
 
 
 if __name__ == "__main__":
