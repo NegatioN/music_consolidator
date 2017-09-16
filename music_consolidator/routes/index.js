9b7e668fafa98ad7089cd7281f4153c30ac6f5d4
Matt Cheah <matt.cheah@gmail.com>
2017-09-16 00:30:43 +0000

    add ajax call to get video title with CSS loader

--- music_consolidator/routes/index.js
+++ music_consolidator/routes/index.js
@@ -13,6 +13,12 @@ router.get('/auth', function(req, res, next) {
     res.render('authenticate', { title: 'Music Consolidator Authenticate' });
 });
 
+router.get('/metadata/:link', function(req, res, next) {
+   var link = req.params.link;
+   
+   res.send(get_metadata(link)); 
+});
+
 router.post('/signup', function(req,res){
     consolidate_music(req.body.link, req.body.artist, req.body.title);
     res.redirect('/');
@@ -32,6 +38,15 @@ function authenticate(auth_code){
     const cli_call = `python ${appDir}/../../authenticate.py --code \"${auth_code}\" --path \"${appDir}/gmusic.creds\"`;
     console.log(cli_call);
     execSync(cli_call);
+}
+
+function get_metadata(link) {
+    console.log("hi");
+    var decoded = decodeURIComponent(link)
+    const cli_call = `youtube-dl --skip-download -e \"${decoded}\"`;
+    
+    var child_process = require('child_process');
+    return child_process.execSync(cli_call).toString();
 }
 
 module.exports = router;
