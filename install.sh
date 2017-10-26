4b62802d968a718125f4ad3739f4bccd6a7a7f7e
NegatioN <joakimrishaug@gmail.com>
2017-10-26 23:45:24 +0200

    also install youtube-dl cli tool

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cd music_consolidator && npm install
 cd .. && pip install -r requirements.txt
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
